9cd2ad9a53c50b9f30b6c4234a043b4022211bd6
Afranur <20170301020@dogus.edu.tr>
2018-03-23 13:00:02 +0300

    HelloWord.
    hw1

---
+++ afranurkose/src/afranurkose/Afranurkose.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afranurkose;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Afranurkose {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello word..");
+    }
+    
+}
